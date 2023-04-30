f3415972e493df88eea792641732bb878c6775b8
Athan <athan.codes@gmail.com>
2023-04-30 18:18:02 +0200

    Exercice correction To see differences

--- app/src/main/java/com/example/quadrantcompose/MainActivity.kt
+++ app/src/main/java/com/example/quadrantcompose/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.quadrantcompose
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.defaultDecayAnimationSpec
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,116 +29,70 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuadrantComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
+                Surface() {
+                    ComposeQuadrantApp()
                 }
             }
         }
     }
 }
 
-// CONTENU DE MON ECRAN :
 @Composable
-fun MyContent() {
-    MyFourQuadrant(
-        title1 = "Text composable",
-        corps1 = "Displays text and follows Material Design guidelines.",
-        color1 = Color.Green,
-
-        title2 = "Row composable",
-        corps2 = "A layout composable that places its children in a horizontal sequence.",
-        color2 = Color.Cyan,
-
-        title3 = "Image composable",
-        corps3 = "Creates a composable that lays out and draws a given Painter class object.",
-        color3 = Color.Yellow,
-
-        title4 = "Column composable",
-        corps4 = "A layout composable that places its children in a vertical sequence.",
-        color4 = Color.LightGray,
-    )
-}
-
-// STRUCTURE D'UN ECRAN 4 QUADRANT :
-@Composable
-fun MyFourQuadrant(
-    title1: String, corps1: String, color1: Color,
-    title2: String, corps2: String, color2: Color,
-    title3: String, corps3: String, color3: Color,
-    title4: String, corps4: String, color4: Color,
-    modifier: Modifier = Modifier
-){
-    Column(modifier = Modifier.fillMaxWidth()){
-        Row(
-            modifier = Modifier
-                .weight(1f),
-            //verticalArrangement = Arrangement.,
-            //horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            MyQuadrant(
-                title = title1,
-                corps = corps1,
-                color = color1,
+fun ComposeQuadrantApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_description),
+                backgroundColor = Color.Green,
                 modifier = Modifier.weight(1f)
             )
-            MyQuadrant(
-                title = title2,
-                corps = corps2,
-                color = color2,
+            ComposableInfoCard(
+                title = stringResource(R.string.second_title),
+                description = stringResource(R.string.second_description),
+                backgroundColor = Color.Yellow,
                 modifier = Modifier.weight(1f)
             )
         }
-        Row(
-            modifier = Modifier
-                .weight(1f),
-            //verticalArrangement = Arrangement.,
-            //horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            MyQuadrant(
-                title = title3,
-                corps = corps3,
-                color = color3,
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.third_title),
+                description = stringResource(R.string.third_description),
+                backgroundColor = Color.Cyan,
                 modifier = Modifier.weight(1f)
             )
-            MyQuadrant(
-                title = title4,
-                corps = corps4,
-                color = color4,
+            ComposableInfoCard(
+                title = stringResource(R.string.fourth_title),
+                description = stringResource(R.string.fourth_description),
+                backgroundColor = Color.LightGray,
                 modifier = Modifier.weight(1f)
             )
         }
     }
 }
 
-
-// STRUCTURE D'UN QUADRANT :
 @Composable
-private fun MyQuadrant(
+private fun ComposableInfoCard(
     title: String,
-    corps: String,
-    color: Color,
-    modifier: Modifier = Modifier)
-{
-    Column (
-        modifier = Modifier
-            .background(color)
-            .padding(16.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = title,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
-            text = corps,
-            //fontSize = default ???
+            text = description,
             textAlign = TextAlign.Justify
         )
     }
@@ -149,24 +100,8 @@ private fun MyQuadrant(
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeQuadrantAppPreview() {
     QuadrantComposeTheme {
-        MyFourQuadrant(
-            title1 = "Text composable",
-            corps1 = "Displays text and follows Material Design guidelines.",
-            color1 = Color.Green,
-
-            title2 = "Row composable",
-            corps2 = "A layout composable that places its children in a horizontal sequence.",
-            color2 = Color.Cyan,
-
-            title3 = "Image composable",
-            corps3 = "Creates a composable that lays out and draws a given Painter class object.",
-            color3 = Color.Yellow,
-
-            title4 = "Column composable",
-            corps4 = "A layout composable that places its children in a vertical sequence.",
-            color4 = Color.LightGray,
-        )
+        ComposeQuadrantApp()
     }
 }
